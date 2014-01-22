ca7566cec8f2184d17054e76c6c3ed6d4e84508a
ktakagaki <kentaroh.takagaki@gmail.com>
2014-01-22 18:01:16 +0100

    140122

--- src/main/scala/nounous/data/Span.scala
+++ src/main/scala/nounous/data/Span.scala
@@ -5,11 +5,21 @@ object Span {
   final val All = new Span(0, -1, 1)
 }
 final case class Span(start: Int, end: Int, step: Int = 1) {
-  require(step != 0)
+  require(step != 0, "step size cannot be zero!")
+
+  def getMaxIndex(length: Int): Int = {
+    val (tempStart, tempEnd) = getActualStartEnd(length)
+    val tempret = math.max(tempStart, tempEnd)
+    require(tempret < length, "maximum index is beyond the given length!")
+    tempret
+  }
+  def getMinIndex(length: Int): Int = {
+    val (tempStart, tempEnd) = getActualStartEnd(length)
+    math.min(tempStart, tempEnd)
+  }
 
   def getRange(length: Int): scala.Range = {
-    val tempStart = if( start<0 ){ length + start } else { start }
-    val tempEnd = if( end<0 ){ length + end } else { end }
+    val (tempStart, tempEnd) = getActualStartEnd(length)
 
     if(step > 0){
       if(tempStart <= tempEnd && tempEnd < length){
@@ -25,6 +35,14 @@ final case class Span(start: Int, end: Int, step: Int = 1) {
       }
     }
   }
+
+  def getActualStartEnd(length: Int): (Int, Int) = {
+    (
+      if( start<0 ){ length + start } else { start },
+      if( end<0 ){ length + end } else { end }
+    )
+  }
+
 
 }
 
--- src/main/scala/nounous/data/xdata/XDataChannelFilestream.scala
+++ src/main/scala/nounous/data/xdata/XDataChannelFilestream.scala
@@ -4,14 +4,22 @@ import nounous.util._
 import nounous.data.Span
 import java.io.{DataInput, File}
 import breeze.io.RandomAccessFileLE
+import nounous.data.traits.XAbsoluteImmutable
 
 /**
  * Created by Kenta on 12/15/13.
  */
-abstract class XDataChannelFilestream extends XDataChannel {
+abstract class XDataChannelFilestream extends XDataChannel with XAbsoluteImmutable {
 
   val fileHandle: DataInput
-  def dataByteLocation(frame: Int): Long
+  override val absGain: Double
+  override val absOffset: Double
+  override val absUnit: String
+  override val segments: Int
+  override val length: Vector[Int]
+  override val startTimestamp: Vector[Long]
+  override val sampleRate: Double
+  override val channelName: String
 
   def readPointImpl(segment: Int, frame: Int): Int
   override def readTraceImpl(segment: Int) = readTraceImpl(segment, Span.All)
