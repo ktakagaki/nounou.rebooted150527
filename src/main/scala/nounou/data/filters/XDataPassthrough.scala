7cae6216f6038aafd09570fbfef8f66cb1c002ce
ktakagaki <kentaroh.takagaki@gmail.com>
2014-02-02 19:01:58 +0100

    Change filtering to Trait

--- src/main/scala/nounou/data/filters/XDataPassthrough.scala
+++ src/main/scala/nounou/data/filters/XDataPassthrough.scala
@@ -5,11 +5,28 @@ import scala.collection.immutable.Vector
 
 /** A passthrough object, which is inherited with various XDataFilter traits to create a filter block.
   */
-class XDataPassthrough( var upstream: XData ) extends XData {
+class XDataPassThrough( var upstream: XData ) extends XData {
+
+  override def channelNames: scala.Vector[String] = upstream.channelNames
+
+  override def readPointImpl(channel: Int, frame: Int, segment: Int): Int = upstream.readPointImpl(channel, frame, segment)
+  override def readTraceImpl(channel: Int, span:Span, segment: Int): Vector[Int] = upstream.readTraceImpl(channel, span, segment)
+  override def readFrameImpl(frame: Int, segment: Int): Vector[Int] = upstream.readFrameImpl(frame, segment)
+  override def readFrameImpl(frame: Int, channels: Vector[Int], segment: Int): Vector[Int] = upstream.readFrameImpl(frame, channels, segment)
+
+  override def absUnit: String = upstream.absUnit
+  override def absOffset: Double = upstream.absOffset
+  override def absGain: Double = upstream.absGain
+
+  override def sampleRate: Double = upstream.sampleRate
+  override def segmentEndTSs: scala.Vector[Long] = upstream.segmentEndTSs
+  override def segmentStartTSs: scala.Vector[Long] = upstream.segmentStartTSs
+  override def segmentLengths: scala.Vector[Int] = upstream.segmentLengths
+  override def segmentCount: Int = upstream.segmentCount
 
   override def isCompatible(target: X) = false
-
   override def :::(target: X): XData = {
-    throw new IllegalArgumentException("cannot append an XDataRepeater!")
+    throw new IllegalArgumentException("cannot append an XDataPassThrough!")
   }
+
 }
