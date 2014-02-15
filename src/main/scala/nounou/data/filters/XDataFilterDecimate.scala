5984c7f9afea87bbfecaed3579b1ab817e94ad63
ktakagaki <kentaroh.takagaki@gmail.com>
2014-02-16 00:30:22 +0100

    Fixed recurrent self reference in XDataFilterDecimate.scala, changed setting mechanism to factor_= Added XDataFilterDownsample.scala

--- src/main/scala/nounou/data/filters/XDataFilterDecimate.scala
+++ src/main/scala/nounou/data/filters/XDataFilterDecimate.scala
@@ -10,36 +10,34 @@
    * @author ktakagaki
    * @date 2/1314.
    */
-  class XDataFilterDecimate(override val upstream: XData ) extends XDataFilter( upstream ) {
+  class XDataFilterDecimate(override val upstream: XData ) extends XDataFilterDownsample( upstream ) {
 
     var kernel: FIRKernel1D[Long] = null
-    var factor: Int = 1
+    override def factor: Int = _factor
+    _factor = 1
 
-    // <editor-fold defaultstate="collapsed" desc=" filter settings ">
+
+    override def factor_= ( factor: Int ): Unit = {
+      require( factor < 16,
+        logger.error( "Downsample rate {} must be < 16." , factor.toString )
+      )
+      if(factor == 1) setDecimateOff()
+      else {
+        kernel = designFilterDecimation[ FIRKernel1D[Long] ](factor, multiplier = 1024L)
+        this._factor = factor
+        logger.info( "set kernel to {}", kernel )
+      }
+    }
 
     def setDecimateOff(): Unit = if(kernel == null){
       logger.info( "filter is already off, not changing. ")
     } else {
       logger.info( "Turning filter kernel off." )
-      factor = 1
+      _factor = 1
       kernel = null
     }
 
-    def setDecimate( factor: Int ): Unit = {
-      require( factor < 16,
-        logger.error( "Downsample rate {} must be < 16." , factor.toString )
-      )
 
-      if(factor == 1) setDecimateOff()
-      else {
-        kernel = designFilterDecimation[ FIRKernel1D[Long] ](factor, multiplier = 1024L)
-        this.factor = factor
-        logger.info( "set kernel to {}", kernel )
-      }
-    }
-
-
-    // </editor-fold>
 
 
     override def readPointImpl(channel: Int, frame: Int, segment: Int): Int =
@@ -66,36 +64,9 @@
         convert( tempRes :/ kernel.multiplier, Int).toVector
       }
 
-    override def readFrameImpl(frame: Int, segment: Int): Vector[Int] = super[XDataFilter].readFrameImpl(frame * factor, segment)
-    override def readFrameImpl(frame: Int, channels: Vector[Int], segment: Int): Vector[Int] = super[XDataFilter].readFrameImpl(frame * factor, channels, segment)
+//    override def readFrameImpl(frame: Int, segment: Int): Vector[Int] = super[XDataFilter].readFrameImpl(frame * factor, segment)
+//    override def readFrameImpl(frame: Int, channels: Vector[Int], segment: Int): Vector[Int] = super[XDataFilter].readFrameImpl(frame * factor, channels, segment)
 
-    //  override def channelNames: scala.Vector[String] = upstream.channelNames
 
-    //  override def absUnit: String = upstream.absUnit
-    //  override def absOffset: Double = upstream.absOffset
-    //  override def absGain: Double = upstream.absGain
-
-    override def sampleRate: Double = upstream.sampleRate / factor
-
-    // override def segmentStartTSs: Vector[Long] = upstream.segmentStartTSs
-    override def segmentEndTSs: Vector[Long] = if( factor == currentSegEndTSFactor ) currentSegEndTSBuffer
-    else {
-      currentSegEndTSBuffer = ( for(seg <- 0 until segmentCount) yield segmentStartTSs(seg) + ((segmentLengths(seg)-1)*tsPerFrame).toLong ).toVector
-      currentSegEndTSFactor = factor
-      currentSegEndTSBuffer
-    }
-    private var currentSegEndTSFactor = 1
-    private var currentSegEndTSBuffer = upstream.segmentEndTSs
-
-    override def segmentLengths: Vector[Int] = if( factor == currentSegLenFactor ) currentSegLenBuffer
-    else {
-      currentSegLenBuffer = ( for(seg <- 0 until segmentCount) yield ( segmentLengths(seg) - 1 )/factor + 1 ).toVector
-      currentSegLenFactor = factor
-      currentSegLenBuffer
-    }
-    private var currentSegLenFactor = 1
-    private var currentSegLenBuffer = upstream.segmentLengths
-
-    //  override def segmentCount: Int = upstream.segmentCount
 
   }
