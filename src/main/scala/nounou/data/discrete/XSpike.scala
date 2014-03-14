61163e4ba1582c8b261c1b3db32fe504e3ad29ed
ktakagaki <kentaroh.takagaki@gmail.com>
2014-03-14 14:16:46 +0100

    spike should return a list of channels

--- src/main/scala/nounou/data/discrete/XSpike.scala
+++ src/main/scala/nounou/data/discrete/XSpike.scala
@@ -3,11 +3,11 @@ package nounou.data
 /**An immutable class to encapsulate a single spike (or n-trode spike), with its waveform, in a neurophysiological recording.
   */
 class XSpike(val waveform: Array[Array[Int]],
-             val channel: Int = -1,
+             val channels: Vector[Int],
              val trode: Int = -1,
              val unit: Int = 0) {
 
-  override def toString = "XSpike( channel="+channel+"unit=" + unit+", trode="+ trode +", waveform.length="+ waveform.length+" )"
+  override def toString = "XSpike( channels="+channels.toString+", unit=" + unit+", trode="+ trode +", waveform.length="+ waveform.length+" )"
 
 
  }
