9aba14998d64133327037276b7beff0430ba1aa8
ktakagaki <kentaroh.takagaki@gmail.com>
2014-04-03 20:10:36 +0200

    RangeTs refactoring

--- src/main/scala/nounou/ranges/RangeTS.scala
+++ src/main/scala/nounou/ranges/RangeTS.scala
@@ -14,28 +14,20 @@ object RangeTs {
     * @param step must amount to a real frame step that is > 0, when converted with the appropriate frame rate
     * @param segment this parameter specifies the recording segment, active in certain file formats
     */
-  def apply(startTs: Long, endTs: Long, step: Long, segment: Int) = new RangeTs(startTs, endTs, step, segment, false)
+  def apply(startTs: Long, endTs: Long, step: Long, segment: Int) = new RangeTs(startTs, endTs, step, segment)
 
   /** Most common constructor, with default segment = 0.
     * @param step must amount to a real frame step that is > 0, when converted with the appropriate frame rate
     */
-  def apply(startTs: Long, endTs: Long, step: Long) = new RangeTs(startTs, endTs, step, 0, false)
+  def apply(startTs: Long, endTs: Long, step: Long) = new RangeTs(startTs, endTs, step)
 
 }
 
-class RangeTs(val startTs: Long, val endTs: Long, val step: Long, val segment: Int, val isAll: Boolean) extends RangeFrSpecifier {
+class RangeTs(val startTs: Long, val endTs: Long, val step: Long, val segment: Int) extends RangeFrSpecifier {
 
-//  def this(startTs: Long, endTs: Long, step: Long, segment: Int) = this(startTs, endTs, step, segment, false)
-//  def this(startTs: Long, endTs: Long, step: Long) = this(startTs, endTs, step, 0, false)
-
-  def this(segment: Int, isAll: Boolean) = this(0L, 0L, 0L, segment, true)
-  def this(isAll: Boolean) = this(0, true)
+  def this(startTs: Long, endTs: Long, step: Long) = this(startTs, endTs, step, 0)
 
   def getFrameRange(x: XFrames): RangeFr = {
-
-    if(isAll){
-      RangeFrAll( /*stepReal,*/ step = 1, segment = segment )
-    } else {
 
       val stepReal = (step * x.sampleInterval/* * 1000d*/).toInt
       loggerRequire(stepReal>0, "This amounts to a negative or zero timestep! (stepMs=" + step + " ms)")
@@ -45,7 +37,6 @@ class RangeTs(val startTs: Long, val endTs: Long, val step: Long, val segment: I
       loggerRequire(startReal._2 == endReal._2, "The two specified timestamps belong to different recording segments {}  and {}.", startReal._2.toString, endReal._2.toString)
 
       new RangeFr(startReal._1, endReal._1, stepReal, segment)
-    }
   }
 
 }
@@ -53,19 +44,26 @@ class RangeTs(val startTs: Long, val endTs: Long, val step: Long, val segment: I
 
 object RangeTsEvent {
 
+  /** Constructor for a frame specification based on timestamps and pre/post data points.
+    * @param preFrames number of data points to take prior to given timestamp. Data points will be taken in steps of `step`
+    * @param postFrames number of data points to take following given timestamp. Data points will be taken in steps of `step`
+    */
+  def apply(eventTs: Long, preFrames: Int, postFrames: Int, step: Int, segment: Int) =
+    new RangeTsEvent(eventTs,  preFrames, postFrames, step, segment)
+
   /** Constructor for a frame specification based on timestamps and pre/post data points, with default segment = 0.
     * @param preFrames number of data points to take prior to given timestamp. Data points will be taken in steps of `step`
     * @param postFrames number of data points to take following given timestamp. Data points will be taken in steps of `step`
     */
-  def apply(eventTS: Long, preFrames: Int, postFrames: Int, step: Int) =
-    new RangeTsEvent(eventTS,  preFrames, postFrames, step, 0)
+  def apply(eventTs: Long, preFrames: Int, postFrames: Int, step: Int) =
+    new RangeTsEvent(eventTs, preFrames, postFrames, step)
 
 }
 
 
 class RangeTsEvent(val eventTS: Long, val preFrames: Int, val postFrames: Int, val step: Int, val segment: Int) extends RangeFrSpecifier {
 
-  //def this(eventTS: Int, preFrames: Int, postFrames: Int, step: Int) = this(eventTS, preFrames, postFrames, step, 0)
+  def this(eventTS: Long, preFrames: Int, postFrames: Int, step: Int) = this(eventTS, preFrames, postFrames, step, 0)
 
   def getFrameRange(x: XFrames): RangeFr = {
     val eventFrame = x.tsToFrsg(eventTS)
